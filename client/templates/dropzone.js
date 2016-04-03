8c9515fdc9ee60ba3d4fcb438e712583eaab90c3
bugrauluyurt <bugra.u@gmail.com>
2016-04-03 14:28:44 +0300

    Finished the dropzone section

---
+++ client/templates/dropzone.js
@@ -0,0 +1,14 @@
+Template.dropzone.events({
+	'dropped #dropzone': function(event) {
+		FS.Utility.eachFile(event, function(file) {
+			var newFile = new FS.File(file);
+			Images.insert(newFile, function(error, result){
+				if (error) {
+					FlashMessages.sendError('There was an issue with the upload');
+				} else {
+					FlashMessages.sendSuccess('Image uploaded');
+				}
+			});
+		});
+	}
+});
